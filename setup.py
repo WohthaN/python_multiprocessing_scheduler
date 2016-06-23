f01bfbf8b661f19beb2e014f262a2db4b5948aa6
Samuele Carli <carlisamuele@csspace.net>
2016-06-23 14:48:08 +0200

    Distutils setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='multiprocessing_scheduler',
+    version='0.1',
+    packages=['', 'test'],
+    url='https://github.com/WohthaN/python_multiprocessing_scheduler',
+    license='GPLv3',
+    author='Carli Samuele',
+    author_email='carlisamuele@csspace.net',
+    description='Generalized load-balanced parallel map() implementation'
+)
